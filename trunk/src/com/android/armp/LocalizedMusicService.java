6316096b4af7d4582026e35ab218ffd312f54104
abarreir <abarreir@0e72a944-42f4-4ded-0146-812fb3def5e8>
2011-03-06 13:59:06 +0000

    Added server connection. Added xml parser for spots.

--- trunk/src/com/android/armp/LocalizedMusicService.java
+++ trunk/src/com/android/armp/LocalizedMusicService.java
@@ -1,44 +1,94 @@
 package com.android.armp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
+import android.provider.SyncStateContract.Constants;
+import android.util.Log;
 
 public class LocalizedMusicService extends Service implements LocationListener {	
 	private LocationManager mLocationMng;
 	private Messenger mClient = null;
-	private ArrayList mSpots;
+	private ArrayList<Spot> mSpots;
+	private static final String TAG = "LocalizedMusicService";
+	private static LocalizedMusicService mInstance = null;
 	
 	public class Spot extends Object {
-		private int i;
+		private double lat;
+		private double lon;
 		
-		public Spot(int val) {
-			i = val;
+		public Spot() {
 		}
 		
-		public int geti() {
-			return i;
+		public Spot(double lat, double lon) {
+			this.lat = lat;
+			this.lon = lon;
+		}
+
+		public void setLat(double lat) {
+			this.lat = lat;
+		}
+
+		public double getLat() {
+			return lat;
+		}
+
+		public void setLon(double lon) {
+			this.lon = lon;
+		}
+
+		public double getLon() {
+			return lon;
 		}
 	};
 	
-	public class Channel {
+	public static Spot createSpot() {
+		return mInstance.new Spot();
+	}
+	
+	public class Channel extends Object {
 		
 	};
 	
-	public class Music {
+	public class Music extends Object {
 		
 	};
 	
@@ -58,10 +108,9 @@ public class LocalizedMusicService extends Service implements LocationListener {
                 case MSG_SPOTS_UPDATE:
                 	if(mClient == null) mClient = msg.replyTo;
                 	
-                	mSpots.add(new Spot(0));
-                	mSpots.add(new Spot(0));
-                	mSpots.add(new Spot(1));
-                	mSpots.add(new Spot(2));
+                	// Request a spot update from the newtork
+                	makeHTTPRequest(MSG_SPOTS_UPDATE, "");
+
                     break;
                 default:
                     super.handleMessage(msg);
@@ -75,22 +124,19 @@ public class LocalizedMusicService extends Service implements LocationListener {
     final Messenger mMessenger = new Messenger(new IncomingHandler());
 
     @Override
-    public void onCreate() {
-    	System.out.println("Service created");
-    	
+    public void onCreate() {    	
     	// Retrieve the LocationManager to enable listening to location updates
         mLocationMng = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         mLocationMng.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, this);
         
+        // Initialize the spots array
         mSpots = new ArrayList<Spot>();
         
+        mInstance = this;
     }
 
     @Override
     public void onDestroy() {
-        // Tell the user we stopped.
-        //Toast.makeText(this, "Service stopped", Toast.LENGTH_SHORT).show();
-        System.out.println("Service stopped");
     }
 
     /**
@@ -100,37 +146,116 @@ public class LocalizedMusicService extends Service implements LocationListener {
     @Override
     public IBinder onBind(Intent intent) {
     	System.out.println("Service bound");
-    	
+    	   	
         return mMessenger.getBinder();
     }
 
 	public void onLocationChanged(Location location) {
-		// TODO Auto-generated method stub
-		try {
-			mClient.send(Message.obtain(null, MSG_SPOTS_UPDATE, (Object) mSpots));	
-		}
-		catch(RemoteException e) {
-			
-		}		
 	}
 
 	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void onProviderEnabled(String provider) {
-		try {
-			mClient.send(Message.obtain(null, MSG_SPOTS_UPDATE, (Object) mSpots));	
-		}
-		catch(RemoteException e) {
-			
-		}
 	}
 
 	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
-		
 	}
+	
+	private static final String baseUrl = "http://abarreir.com/";
+	private static final String userAgent = "";
+	
+	/**
+	 * 
+	 * @param reqId The kind of request we are performing
+	 */
+	private void makeHTTPRequest(final int reqId, final String params) {
+		// Fire off a thread to do some work that we shouldn't do directly in the UI thread
+		Thread t = new Thread() {
+			public void run() {
+				try {
+					// Create an http client
+					AndroidHttpClient httpclient =  AndroidHttpClient.newInstance(userAgent);
+					
+					// Generate the string regarding the reqId
+					String requestURL = baseUrl;
+					Object response = null;
+					
+					switch(reqId) {
+					case MSG_SPOTS_UPDATE:
+						Log.i(TAG, "Updating spots");
+						requestURL += "dbg.php";
+						
+						// Reset the current spots
+						mSpots.clear();
+						
+						// Update the spots with the result of the response handler
+						HttpGet httpget = new HttpGet(requestURL);
+						response = mSpots = httpclient.execute(httpget, spotsResponseHandler);
+						
+						break;
+					}
+					
+					// Send the answer to the activity
+					Message msg = Message.obtain(null, 
+										LocalizedMusicService.MSG_SPOTS_UPDATE, 
+										response);
+					mClient.send(msg);
+					
+					// Finally, close the client to avoid any leak
+					httpclient.close();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		t.start();
+	}
+	
+	private ResponseHandler<ArrayList<Spot> > spotsResponseHandler = new ResponseHandler<ArrayList<Spot> >() {
+		//@Override
+		public ArrayList<Spot> handleResponse(HttpResponse r) {
+			String result = "";
+			try {
+				// First, we retrieve the xml string from the server
+				BufferedReader br = new BufferedReader(new InputStreamReader(r.getEntity().getContent()));
+				String line;
+				while ((line = br.readLine()) != null)
+	               result += line;
+				
+				Log.d(TAG, result);
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+			
+			ArrayList<Spot> res = null;
+			
+			// Then, we parse the retrieved string
+			try {
+				/* Get a SAXParser from the SAXPArserFactory. */
+	            SAXParserFactory spf = SAXParserFactory.newInstance();
+	            SAXParser sp = spf.newSAXParser();
 
+	            /* Get the XMLReader of the SAXParser we created. */
+	            XMLReader xr = sp.getXMLReader();
+	            
+	            /* Create a new ContentHandler and apply it to the XML-Reader*/
+	            SpotsXMLHandler spotsHandler = new SpotsXMLHandler();
+	            xr.setContentHandler(spotsHandler);
+	           
+	            /* Parse the xml-data from our string*/
+	            xr.parse(new InputSource(new StringReader(result)));
+
+	            /* Our ExampleHandler now provides the parsed data to us. */
+	            res = spotsHandler.getParsedData();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			return res;
+		}
+	};
 }
