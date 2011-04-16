b306354d2d944694270e7ca2d4f6ba4eedf818f3
fabienrenaud.fr@gmail.com <fabienrenaud.fr@gmail.com@0e72a944-42f4-4ded-0146-812fb3def5e8>
2011-04-16 15:51:57 +0000

    First facebook connection should now authenticate the user on our own host.

--- trunk/src/com/android/armp/localized/SmartMapView.java
+++ trunk/src/com/android/armp/localized/SmartMapView.java
@@ -1,14 +1,9 @@
 package com.android.armp.localized;
 
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.location.Location;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 
 import com.google.android.maps.GeoPoint;
@@ -18,7 +13,7 @@ public class SmartMapView extends MapView {
 	private static final String TAG = "SmartMapView";
 	private static final long CHECK_DELAY = 2000;
 	private static final double MOTION_TRESHOLD = 3.0;
-	
+
 	private GeoPoint mPrevCenter;
 	private boolean inMotion;
 	private boolean isTouching;
@@ -28,63 +23,60 @@ public class SmartMapView extends MapView {
 		super(arg0, arg1);
 		mPrevCenter = new GeoPoint(0, 0);
 	}
-	
+
 	public interface OnAreaChangedListener {
-        void onAreaChanged(GeoPoint topLeft, GeoPoint bottomRight);
+		void onAreaChanged(GeoPoint topLeft, GeoPoint bottomRight);
 	}
-	
+
 	public void setOnAreaChangedListener(OnAreaChangedListener l) {
 		mListener = l;
 	}
-	
+
 	@Override
 	public void dispatchDraw(Canvas canvas) {
 		super.dispatchDraw(canvas);
-		
+
 		GeoPoint p = getMapCenter();
 		double dist = distanceBetween(p, mPrevCenter);
 
 		// If the user stopped moving around the map
-		if(dist < MOTION_TRESHOLD && inMotion && !isTouching) {
+		if (dist < MOTION_TRESHOLD && inMotion && !isTouching) {
 			inMotion = false;
-			int latSpanCenter = getLatitudeSpan()/2;
-			int lonSpanCenter = getLongitudeSpan()/2;
+			int latSpanCenter = getLatitudeSpan() / 2;
+			int lonSpanCenter = getLongitudeSpan() / 2;
 			int lat = p.getLatitudeE6();
 			int lon = p.getLongitudeE6();
-			
-			GeoPoint ne = new GeoPoint(lat+latSpanCenter,
-										lon+lonSpanCenter);
-			GeoPoint sw = new GeoPoint(lat-latSpanCenter,
-										lon-lonSpanCenter);
-			
+
+			GeoPoint ne = new GeoPoint(lat + latSpanCenter, lon + lonSpanCenter);
+			GeoPoint sw = new GeoPoint(lat - latSpanCenter, lon - lonSpanCenter);
+
 			// Notify the listener
-			mListener.onAreaChanged(ne, sw);			
-		} 
-		else if (dist >= MOTION_TRESHOLD) {
+			mListener.onAreaChanged(ne, sw);
+		} else if (dist >= MOTION_TRESHOLD) {
 			mPrevCenter = p;
 			inMotion = true;
 		}
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent e) {
-		if(e.getAction() == MotionEvent.ACTION_DOWN)
+		if (e.getAction() == MotionEvent.ACTION_DOWN)
 			isTouching = true;
-		else if(e.getAction() == MotionEvent.ACTION_UP)
+		else if (e.getAction() == MotionEvent.ACTION_UP)
 			isTouching = false;
-		
+
 		return super.onTouchEvent(e);
 	}
-	
+
 	private double distanceBetween(GeoPoint p1, GeoPoint p2) {
-		float[] res = {0.0f};
-		double lat1 = p1.getLatitudeE6()/1E6;
-		double lat2 = p2.getLatitudeE6()/1E6;
-		double lon1 = p1.getLongitudeE6()/1E6;
-		double lon2 = p2.getLongitudeE6()/1E6;
-				
+		float[] res = { 0.0f };
+		double lat1 = p1.getLatitudeE6() / 1E6;
+		double lat2 = p2.getLatitudeE6() / 1E6;
+		double lon1 = p1.getLongitudeE6() / 1E6;
+		double lon2 = p2.getLongitudeE6() / 1E6;
+
 		Location.distanceBetween(lat1, lon1, lat2, lon2, res);
-		
-		return (res[0]*1E4)/getLatitudeSpan();
+
+		return (res[0] * 1E4) / getLatitudeSpan();
 	}
 }
